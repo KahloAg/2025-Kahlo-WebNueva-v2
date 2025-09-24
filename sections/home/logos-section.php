3f9a737f5afe9aff979463230277dfa0fb91f89b
Florencia Fiorentino <69087451+florfioren@users.noreply.github.com>
2025-09-24 14:10:01 -0300

    ka

--- sections/home/logos-section.php
+++ sections/home/logos-section.php
@@ -4,9 +4,9 @@
         <div class="container h-100">
             <div class="row justify-content-center align-items-center h-100">
                 <div class="col-xl-11 d-flex align-items-center flex-column h-100 justify-content-center text-center">
-                    <p class="before-title lila mb-5 w1000">A LA EXCELENCIA, Y MÁS ALLÁ!</p>
+                    <p class="before-title lila mb-5 fw-bold">A LA EXCELENCIA, Y MÁS ALLÁ!</p>
                     <h3>Avanzamos cada proyecto midiendo la <i class="lila">satisfacción</i> de nuestros clientes</h3>
-                    <p class="before-title w600 mt-5 mb-0">en:&nbsp;&nbsp; Atención + Tiempo + Creatividad + Estrategia
+                    <p class="before-title mt-5 mb-0">en:&nbsp;&nbsp; Atención + Tiempo + Creatividad + Estrategia
                     </p>
                 </div>
             </div>
