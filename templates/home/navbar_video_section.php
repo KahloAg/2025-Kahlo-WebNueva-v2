32291882eca88d4b12326908a34d13863536772c
Florencia Fiorentino <69087451+florfioren@users.noreply.github.com>
2025-10-28 15:15:21 -0300

    img

--- templates/home/navbar_video_section.php
+++ templates/home/navbar_video_section.php
@@ -1,6 +1,6 @@
     <!-- header area end -->
     <div class="rts-banner-area-7 bg_image rts-scroll-down-circle-wrapper-3 degrade vh-100">
-        <video loop muted autoplay playsinline class="video-bg" poster="img/bg-home.png">
+        <video loop muted autoplay playsinline class="video-bg" poster="img/bg-home.jpg">
             <source src="img/bg-home.mp4" type="video/mp4">
         </video>
         <div class="black-overlay"></div>
