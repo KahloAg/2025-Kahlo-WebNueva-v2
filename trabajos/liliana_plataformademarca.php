e443e5046478cc3bef8dfa98e022652d11950470
Florencia Fiorentino <69087451+florfioren@users.noreply.github.com>
2026-01-12 20:12:25 -0300

    tigo

---
+++ trabajos/liliana_plataformademarca.php
@@ -0,0 +1,205 @@
+<?php
+    include_once("../_general.php");
+?>
+
+
+<?php include_once("../templates/trabajos/head-info.php"); ?>
+</head>
+
+<body class="index-six">
+
+   <?php include_once("../templates/home/navbar.php"); ?>
+
+    <main>
+        <section class="d-flex align-items-center min-hv">
+            <div class="container mt-83">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-8">
+                        <div class="d-flex align-items-center justify-content-between col-xl-8">
+                            <img src="../img/liliana.svg" class="logo-interna">
+                            <p class="text-white font13 mb-0">/PÚBLICO EXTERNO/</p>
+                        </div>
+                        <h1 class="mt-6 mb-0">Liliana <br> Plataforma de marca</h1>
+                    </div>
+                    <div class="col-9 col-sm-8 col-lg-3 offset-lg-9 mt-5 mt-lg-0">
+                        <p class="text-white text-uppercase font13 lh-21">Plataforma de marca que ordena el portafolio y pone en valor su presencia cotidiana.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 embed-container">
+                        <iframe src="https://player.vimeo.com/video/1148713610?autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;muted=1&amp;potrait=0&amp;transparent=0" style="width: 100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacer my-50">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 text-center txt-trabajo-sub mt-7 mb-0">
+                        <p class="text-white mb-0">EL DESAFÍO <br> Liliana es una de las marcas de electrodomésticos más presentes en los hogares argentinos. Sin embargo, esa fortaleza no siempre se reflejaba en una <b>comunicación de marca unificada</b>.<br><br>
+El desafío fue ordenar la diversidad del portafolio y construir una plataforma conceptual consistente, flexible y sostenible en el tiempo.
+                        </p>
+
+                        
+                    </div>
+                        </div>
+                        </div>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                <img src="../img/trabajos/liliana_rebranding-1.gif" class="img-fluid">
+                                </div>
+                                <div class="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 text-center txt-trabajo-sub mb-7">
+
+                                <p class="text-white mb-0">EL INSIGHT<br>
+                            Las personas no ven a Liliana como un producto puntual, sino como una marca que acompaña su vida cotidiana.<br>
+En muchos hogares, Liliana no está en un solo lugar: <b>está en todo.</b><br>
+Su valor no está en exagerar atributos, sino en <b>estar presente en los momentos reales del día a día</b>.</p>
+                                </div>
+                                <div class="col-12 mt-7">
+                                <img src="../img/trabajos/liliana_rebranding-2.jpg" class="img-fluid">
+                                </div>
+                        </div>
+                        </div>
+
+
+                       <div class="container mt-7">
+                       <div class="row align-items-center">
+                       <div class="col-12 col-lg-5 col-xl-4">
+                       <p class="text-white mb-0">LA IDEA Y LA ESTRATEGIA<br>
+                           De ese insight nació <b>“Tu marca en todo”</b>, una plataforma de marca que unifica categorías, refuerza el vínculo emocional y convive con comunicación institucional y comercial.<br>
+Desde Kahlo la desarrollamos como una <b>marca madre</b>, con productos integrados en escenas reales, un tono cercano y hogares vividos y reconocibles.</p>
+                    </div>
+
+                    <div class="col-12 col-lg-6 offset-lg-1 offset-xl-2 mt-7 mt-lg-0">
+                        <img src="../img/trabajos/liliana_rebranding-3.gif" class="img-fluid">
+                    </div>
+                        </div>
+                        </div>
+
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                <img src="../img/trabajos/liliana_rebranding-4.gif" class="img-fluid">
+                                </div>
+
+                                
+                         <div class="col-12 mt-5">
+                        <img src="../img/trabajos/liliana_rebranding-5.jpg" class="img-fluid">
+                    </div>
+
+                     <div class="col-12 mt-7 mb-7">
+                        <img src="../img/trabajos/liliana_rebranding-6.jpg" class="img-fluid">
+                    </div>
+
+                    <div class="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 text-center txt-trabajo-sub mb-15">
+                                     <p class="text-white mb-0">EL RESULTADO<br>
+                           <b>“Tu marca en todo”</b> se consolidó como una declaración de marca que ordena y proyecta a Liliana hacia el futuro, respetando su ADN histórico.<br>
+Una plataforma <b>sólida y escalable</b>, que potencia su principal diferencial: <b>estar presente en la vida cotidiana de millones de personas.</b></p>
+                        </div>
+
+                             
+                        </div>
+                        </div>
+
+        </section>
+
+        <?php include_once("../templates/trabajos/anterior-siguiente-section.php"); ?>
+
+        <div id="footer-spacer" aria-hidden="true"></div>
+    </main>
+
+    <?php include_once("../templates/home/footer.php"); ?>
+
+   
+
+    <script defer src="../js/jquery.min.js"></script>
+    <script defer src="../js/bootstrap.min.js"></script>
+    <script defer src="../js/waypoint.js"></script>
+    <script defer src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/gsap.js"></script>
+    <script defer src="../js/smoothscroll-varticle.js"></script>
+    <script src="../js/smoothscroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script defer src="../js/scrolltoplugin.js"></script>
+    <script defer src="../js/splittext.js"></script>
+    <script defer src="../js/counterup.js"></script>
+    <script defer src="../js/waw.js"></script>
+    <script defer src="../js/main.js"></script>
+
+    <script>
+        const menuToggle = document.getElementById("menuToggle");
+        const mobileNav = document.getElementById("mobileNav");
+        const mobileLinks = mobileNav.querySelectorAll("a");
+        const header = document.querySelector("header");
+
+        menuToggle.addEventListener("click", () => {
+            menuToggle.classList.toggle("active");
+            mobileNav.classList.toggle("open");
+        });
+
+        mobileLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                menuToggle.classList.remove("active");
+                mobileNav.classList.remove("open");
+            });
+        });
+
+        const mediaQuery = window.matchMedia("(max-width: 992px)");
+
+        function handleScroll() {
+            if (mediaQuery.matches) {
+                if (window.scrollY > 50) {
+                    header.classList.add("scrolled");
+                } else {
+                    header.classList.remove("scrolled");
+                }
+            } else {
+                header.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
+        handleScroll();
+
+        function ajustarAlturaFooter() {
+            const footer = document.querySelector("footer.footer");
+            const main = document.querySelector("main");
+            if (footer && main) {
+                const footerHeight = footer.offsetHeight;
+                main.style.marginBottom = footerHeight + "px";
+            }
+        }
+
+        window.addEventListener("load", ajustarAlturaFooter);
+        window.addEventListener("resize", ajustarAlturaFooter);
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const items = document.querySelectorAll("footer .fade-item");
+
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add("visible");
+                        }
+                    });
+                },
+                {
+                    root: null,
+                    threshold: 0.1,
+                }
+            );
+
+            items.forEach((item) => observer.observe(item));
+        });
+    </script>
+</body>
+</html>
---
+++ trabajos/tigo_villantigos.php
@@ -0,0 +1,225 @@
+<?php
+    include_once("../_general.php");
+?>
+
+
+<?php include_once("../templates/trabajos/head-info.php"); ?>
+</head>
+
+<body class="index-six">
+
+   <?php include_once("../templates/home/navbar.php"); ?>
+
+    <main>
+        <section class="d-flex align-items-center min-hv">
+            <div class="container mt-83">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-8">
+                        <div class="d-flex align-items-center justify-content-between col-xl-8">
+                            <img src="../img/tigo.svg" class="logo-interna">
+                            <p class="text-white font13 mb-0">/PÚBLICO EXTERNO/</p>
+                        </div>
+                        <h1 class="mt-6 mb-0">Tigo <br> Villantigos</h1>
+                    </div>
+                    <div class="col-9 col-sm-8 col-lg-3 offset-lg-9 mt-5 mt-lg-0">
+                        <p class="text-white text-uppercase font13 lh-21">Campaña 360° de Navidad inspirada en villancicos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 embed-container">
+                        <iframe src="https://player.vimeo.com/video/1150129750?autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;muted=1&amp;potrait=0&amp;transparent=0" style="width: 100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacer my-50">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-7 mb-15">
+                    <p class="text-white mb-0">EL INSIGHT <br><br> Tigo buscaba posicionarse como la mejor opción para regalar en Navidad, reforzando su propuesta de valor: precio justo y una experiencia integral de conectividad. El desafío fue construir una campaña emocional que fuera más allá de comunicar promociones, conectando con el espíritu familiar de la temporada para generar cercanía y recordación.</p>                     
+                    </div>
+
+                    <div class="col-12 col-lg-5 col-xl-5">
+                    <video width="100%" height="" loop autoplay muted>
+                    <source src="../img/trabajos/tigo_villantigos-5.mp4" type="video/mp4">
+                    Tu navegador no soporta el elemento de video.
+                    </video>
+                    </div>
+
+                    <div class="col-12 col-lg-6 offset-lg-1 mt-7 mt-lg-0 txt-trabajo-sub">
+                   
+                    <p class="text-white mb-0">Los villancicos son un código navideño transversal en Norte, Centro y el norte de Sudamérica: canciones populares que se cantan en familia, suenan en la calle, en la radio y en reuniones, y funcionan como un “lenguaje común” de la época. <br><br>La pregunta fue simple: ¿y si la promo de Tigo se convirtiera en el verdadero hit de la Navidad?</p>
+                    </div>
+
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-15 mb7">
+                    <p class="text-white mb-0">Así nació “Los Villantigos”: una serie de piezas cálidas, divertidas y musicales que reversionaban clásicos navideños con letras creadas para contar los beneficios de Full Tigo, convirtiendo funcionalidades del producto en momentos compartidos.
+                    </p>                     
+                    </div>
+                    
+
+                   
+                    </div>
+                    </div>
+
+                    <div class="container-fluid">
+                    <div class="row justify-content-center">
+                    <div class="col-11 mt-7">
+                    <img src="../img/trabajos/tigo_villantigos-1.png" class="img-fluid">
+                    </div>
+                    </div>
+                    </div>
+
+                    <div class="container">
+                    <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-15 mb-7">
+                    <p class="text-white mb-0">LA ESTRATEGIA<br><br> Diseñamos una experiencia 360°. En digital, los “temazos” se amplificaron con formatos participativos (reels, carruseles tipo karaoke y dinámicas para cantar). <br><br>En tiendas, la campaña cobró vida con karaoke en vivo, espacios instagrameables y ambientación temática, apoyada por creadores para impulsar la visita y la conversación.</p>                     
+                    </div>
+
+                    <div class="col-12 mt-7">
+                    <img src="../img/trabajos/tigo_villantigos-6.jpg" class="img-fluid rounded-4">
+                    </div>
+
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-15 mb-7">
+                    <p class="text-white mb-0">RESULTADOS<br><br> Se registró un aumento del tráfico tanto en tiendas como en canales digitales durante la semana previa a Navidad, impulsado por una ejecución que convirtió la promo en experiencia.</p>                     
+                    </div>
+
+                    </div>
+                    </div>
+
+                   <div class="container-fluid">
+                    <div class="row justify-content-center">
+                    <div class="col-12 col-md-3 mt-7">
+                    <video width="100%" height="" loop autoplay muted>
+                    <source src="../img/trabajos/tigo_villantigos-2.mp4" type="video/mp4">
+                    Tu navegador no soporta el elemento de video.
+                    </video>
+                    </div>
+
+                    <div class="col-12 col-md-3 mt-7">
+                    <video width="100%" height="" loop autoplay muted>
+                    <source src="../img/trabajos/tigo_villantigos-3.mp4" type="video/mp4">
+                    Tu navegador no soporta el elemento de video.
+                    </video>
+                    </div>
+
+                    <div class="col-12 col-md-3 mt-7">
+                    <video width="100%" height="" loop autoplay muted>
+                    <source src="../img/trabajos/tigo_villantigos-4.mp4" type="video/mp4">
+                    Tu navegador no soporta el elemento de video.
+                    </video>
+                    </div>
+                    </div>
+                    </div>
+
+                    <div class="container">
+                    <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-15 mb-15">
+                    <p class="text-white mb-0">Combinamos creatividad, estrategia y cultura local para conectar a las personas con Full Tigo en el momento más emocional del año.</p>                     
+                    </div>
+
+                    </div>
+                    </div>
+
+                          
+
+        </section>
+
+        <?php include_once("../templates/trabajos/anterior-siguiente-section.php"); ?>
+
+        <div id="footer-spacer" aria-hidden="true"></div>
+    </main>
+
+    <?php include_once("../templates/home/footer.php"); ?>
+
+   
+
+    <script defer src="../js/jquery.min.js"></script>
+    <script defer src="../js/bootstrap.min.js"></script>
+    <script defer src="../js/waypoint.js"></script>
+    <script defer src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/gsap.js"></script>
+    <script defer src="../js/smoothscroll-varticle.js"></script>
+    <script src="../js/smoothscroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script defer src="../js/scrolltoplugin.js"></script>
+    <script defer src="../js/splittext.js"></script>
+    <script defer src="../js/counterup.js"></script>
+    <script defer src="../js/waw.js"></script>
+    <script defer src="../js/main.js"></script>
+
+    <script>
+        const menuToggle = document.getElementById("menuToggle");
+        const mobileNav = document.getElementById("mobileNav");
+        const mobileLinks = mobileNav.querySelectorAll("a");
+        const header = document.querySelector("header");
+
+        menuToggle.addEventListener("click", () => {
+            menuToggle.classList.toggle("active");
+            mobileNav.classList.toggle("open");
+        });
+
+        mobileLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                menuToggle.classList.remove("active");
+                mobileNav.classList.remove("open");
+            });
+        });
+
+        const mediaQuery = window.matchMedia("(max-width: 992px)");
+
+        function handleScroll() {
+            if (mediaQuery.matches) {
+                if (window.scrollY > 50) {
+                    header.classList.add("scrolled");
+                } else {
+                    header.classList.remove("scrolled");
+                }
+            } else {
+                header.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
+        handleScroll();
+
+        function ajustarAlturaFooter() {
+            const footer = document.querySelector("footer.footer");
+            const main = document.querySelector("main");
+            if (footer && main) {
+                const footerHeight = footer.offsetHeight;
+                main.style.marginBottom = footerHeight + "px";
+            }
+        }
+
+        window.addEventListener("load", ajustarAlturaFooter);
+        window.addEventListener("resize", ajustarAlturaFooter);
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const items = document.querySelectorAll("footer .fade-item");
+
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add("visible");
+                        }
+                    });
+                },
+                {
+                    root: null,
+                    threshold: 0.1,
+                }
+            );
+
+            items.forEach((item) => observer.observe(item));
+        });
+    </script>
+</body>
+</html>
