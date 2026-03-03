58b7b2757b5f1181cec28f70ceeaa5123518a793
Florencia Fiorentino <69087451+florfioren@users.noreply.github.com>
2026-03-03 15:24:38 -0300

    ff

---
+++ trabajos/ypf_comunicacioninterna.php
@@ -0,0 +1,200 @@
+<?php
+    include_once("../_general.php");
+?>
+
+
+<?php include_once("../templates/trabajos/head-info.php"); ?>
+</head>
+
+<body class="index-six">
+
+   <?php include_once("../templates/home/navbar.php"); ?>
+
+    <main>
+        <section class="d-flex align-items-center min-hv">
+            <div class="container mt-83">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-8">
+                        <div class="d-flex align-items-center justify-content-between col-xl-8">
+                            <img src="../img/ypf.png" class="logo-interna">
+                            <p class="text-white font13 mb-0">/PÚBLICO INTERNO/</p>
+                        </div>
+                        <h1 class="mt-6 mb-0">YPF <br> Gestión integral de Comunicación Interna</h1>
+                    </div>
+                    <div class="col-9 col-sm-8 col-lg-3 offset-lg-9 mt-5 mt-lg-0">
+                        <p class="text-white text-uppercase font13 lh-21">DESARROLLO DE COMUNICACIÓN INTERNA PARA GENERAR CONEXIÓN CON LOS COLABORADORES</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 p-0">
+                     <img src="../img/trabajos/ypf_comuinterna-1.jpg" class="img-fluid">    
+                    </div>
+                    <div class="col-12 embed-container">
+                        <iframe src="https://player.vimeo.com/video/1169994123?autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;muted=1&amp;potrait=0&amp;transparent=0" style="width: 100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacer my-50">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 text-start txt-trabajo-sub mt-7 mb-15">
+                    <p class="text-white mb-0">EL DESAFÍO <br><br> YPF necesitaba una Comunicación Interna más estratégica: menos dispersión de mensajes entre áreas, más coherencia entre campañas y una narrativa común que construyera cultura, pertenencia y alineación con el negocio.</p>                     
+                    </div>
+
+                    <div class="row justify-content-center mb-7">
+                        <div class="col-12 col-lg-4 txt-trabajo-sub">
+                    <p class="text-white mb-0">LA GESTIÓN <br class="d-none d-lg-block">INTEGRAL</p>  
+                        </div>
+                    <div class="col-12 col-lg-6 ">
+                    <p class="text-white mb-0">Desde Kahlo Agencia diseñamos una gestión end-to-end de comunicación interna que abarcó campañas culturales, programas de talento y proyectos de diversidad e inclusión bajo un mismo sistema. Relanzamos #SientoYPF como paraguas de cultura y orgullo, creamos la arquitectura creativa del Programa de incorporación 2025, y desarrollamos la identidad visual integral de la Red de Mujeres YPF, siempre con narrativa, tono y diseño alineados.</p>
+                    </div>
+
+                  
+                    </div>
+
+                    <div class="row justify-content-center mb-15">
+                        <div class="col-12 col-lg-4 txt-trabajo-sub">
+                    <p class="text-white mb-0">SEGMENTAMOS <br class="d-none d-lg-block">AUDIENCIAS</p>  
+                        </div>
+                    <div class="col-12 col-lg-6">
+                    <p class="text-white mb-0">Mapeamos pain points por área y definimos mensajes clave, códigos visuales y calls to action por objetivo, evitando superposiciones entre iniciativas como Pasantías y otros programas internos. De esta forma, cada campaña dejó de vivir aislada y empezó a sumar a un relato interno consistente, fácil de reconocer y de explicar dentro de la organización.</p>
+                    </div>
+
+                  
+                    </div>
+                    
+
+                   
+                    </div>
+                    </div>
+
+                    <div class="container">
+                    <div class="row justify-content-center">
+                    <div class="col-12 col-md-6 mt-4">
+                    <img src="../img/trabajos/ypf_comuinterna-2.jpg" class="img-fluid">
+                    </div>
+                    <div class="col-12 col-md-6 mt-4">
+                    <img src="../img/trabajos/ypf_comuinterna-3.jpg" class="img-fluid">
+                    </div>
+                    <div class="col-12 mt-4">
+                    <img src="../img/trabajos/ypf_comuinterna-4.jpg" class="img-fluid">
+                    </div>
+                    <div class="col-12 col-md-6 mt-4">
+                    <img src="../img/trabajos/ypf_comuinterna-5.jpg" class="img-fluid">
+                    </div>
+                    <div class="col-12 col-md-6 mt-4">
+                    <img src="../img/trabajos/ypf_comuinterna-6.jpg" class="img-fluid">
+                    </div>
+
+                    <div class="col-12 col-lg-10 text-start txt-trabajo-sub mt-15 mb-15">
+                    <p class="text-green">RESULTADOS</p>
+                    <p class="text-white mb-0">Las campañas ganaron consistencia, recordación y claridad: cada área comenzó a comunicarse bajo un mismo sistema, fortaleciendo el rol de Comunicación Interna como partner estratégico del negocio. Las iniciativas clave de cultura y comunicación lograron mayor identificación, más participación y mejores indicadores de engagement interno.</p>                     
+                    </div>
+                    </div>
+                    </div>
+
+
+                          
+
+        </section>
+
+        <?php include_once("../templates/trabajos/anterior-siguiente-section.php"); ?>
+
+        <div id="footer-spacer" aria-hidden="true"></div>
+    </main>
+
+    <?php include_once("../templates/home/footer.php"); ?>
+
+   
+
+    <script defer src="../js/jquery.min.js"></script>
+    <script defer src="../js/bootstrap.min.js"></script>
+    <script defer src="../js/waypoint.js"></script>
+    <script defer src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/gsap.js"></script>
+    <script defer src="../js/smoothscroll-varticle.js"></script>
+    <script src="../js/smoothscroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script defer src="../js/scrolltoplugin.js"></script>
+    <script defer src="../js/splittext.js"></script>
+    <script defer src="../js/counterup.js"></script>
+    <script defer src="../js/waw.js"></script>
+    <script defer src="../js/main.js"></script>
+
+    <script>
+        const menuToggle = document.getElementById("menuToggle");
+        const mobileNav = document.getElementById("mobileNav");
+        const mobileLinks = mobileNav.querySelectorAll("a");
+        const header = document.querySelector("header");
+
+        menuToggle.addEventListener("click", () => {
+            menuToggle.classList.toggle("active");
+            mobileNav.classList.toggle("open");
+        });
+
+        mobileLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                menuToggle.classList.remove("active");
+                mobileNav.classList.remove("open");
+            });
+        });
+
+        const mediaQuery = window.matchMedia("(max-width: 992px)");
+
+        function handleScroll() {
+            if (mediaQuery.matches) {
+                if (window.scrollY > 50) {
+                    header.classList.add("scrolled");
+                } else {
+                    header.classList.remove("scrolled");
+                }
+            } else {
+                header.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
+        handleScroll();
+
+        function ajustarAlturaFooter() {
+            const footer = document.querySelector("footer.footer");
+            const main = document.querySelector("main");
+            if (footer && main) {
+                const footerHeight = footer.offsetHeight;
+                main.style.marginBottom = footerHeight + "px";
+            }
+        }
+
+        window.addEventListener("load", ajustarAlturaFooter);
+        window.addEventListener("resize", ajustarAlturaFooter);
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const items = document.querySelectorAll("footer .fade-item");
+
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add("visible");
+                        }
+                    });
+                },
+                {
+                    root: null,
+                    threshold: 0.1,
+                }
+            );
+
+            items.forEach((item) => observer.observe(item));
+        });
+    </script>
+</body>
+</html>
