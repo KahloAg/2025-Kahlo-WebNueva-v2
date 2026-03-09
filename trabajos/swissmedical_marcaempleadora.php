2f865ffc910ed9575faa9a61a50e06bd59667ae2
Florencia Fiorentino <69087451+florfioren@users.noreply.github.com>
2026-03-09 13:23:06 -0300

    swiss

---
+++ trabajos/swissmedical_marcaempleadora.php
@@ -0,0 +1,228 @@
+<?php
+    include_once("../_general.php");
+?>
+
+
+<?php include_once("../templates/trabajos/head-info.php"); ?>
+</head>
+
+<body class="index-six">
+
+   <?php include_once("../templates/home/navbar.php"); ?>
+
+    <main>
+        <section class="d-flex align-items-center min-hv">
+            <div class="container mt-83">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-8">
+                        <div class="d-flex align-items-center justify-content-between col-xl-8">
+                            <img src="../img/tigo.svg" class="logo-interna">
+                            <p class="text-white font13 mb-0">/MARCA EMPLEADORA/</p>
+                        </div>
+                        <h1 class="mt-6 mb-0">Swiss Medical <br>
+marca empleadora</h1>
+                    </div>
+                    <div class="col-9 col-sm-8 col-lg-3 offset-lg-9 mt-5 mt-lg-0">
+                        <p class="text-white text-uppercase font13 lh-21">Estrategia y creatividad con impacto.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+  
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                       <img src="../img/trabajos/swissmedical_marcaempleadora-1.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="spacer">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 text-center  mb-15">
+                    <p class="text-white">EL DESAFÍO</p>
+                    <p class="text-white mb-0 txt-trabajo-sub"> Swiss Medical Group tenía el objetivo de posicionarse como una de las marcas empleadoras más admiradas del país. Para lograrlo, necesitaba fortalecer su posicionamiento, atraer talento de calidad y visibilizar su amplio ecosistema de negocios y oportunidades de desarrollo</p>                     
+                    </div>
+
+                    <div class="col-10 offset-1 offset-md-0 col-lg-5 col-md-6 col-xl-4 mt-7 mt-lg-0 order-2 order-md-1 text-center text-md-start">
+                   
+                    <p class="text-white mb-0">01. <br><br>Realizamos un análisis profundo de la organización y su entorno, identificando percepciones, fortalezas y áreas de mejora. Este diagnóstico permitió definir una estrategia sólida de employer branding con foco en propósito, coherencia y conexión emocional.
+                    </p>
+                    </div>
+
+                    <div class="col-10 offset-1 offset-md-0 col-md-6 offset-lg-1 offset-xl-2 mt-7 mt-lg-0 order-1 order-md-2">
+                    <img src="../img/trabajos/swissmedical_marcaempleadora-4.gif" class="img-fluid">
+                    </div>
+
+                    
+
+                    <div class="col-10 offset-1 offset-md-0 col-md-6 mt-7 order-3">
+                    <img src="../img/trabajos/swissmedical_marcaempleadora-2.png" class="img-fluid">
+                    </div>
+
+                    <div class="col-10 offset-1 offset-md-0 col-md-6 col-lg-5 offset-lg-1 offset-xl-2 col-xl-4 mt-7 order-4 text-center text-md-start">
+                   
+                    <p class="text-white mb-0">02. <br><br>Diseñamos una matriz de contenidos y estética visual que le dio identidad a cada atributo de la marca. Creamos narrativas específicas para áreas clave y desarrollamos una comunicación cercana, aspiracional y coherente con los valores del Grupo.</p>
+                    </div>
+
+                    
+                    
+
+                   
+                    </div>
+                    </div>
+
+                    <div class="container mt-7">
+                <div class="row justify-content-center">
+                    <div class="col-11 embed-container">
+                        <iframe src="https://player.vimeo.com/video/1171754082?autoplay=1&amp;loop=1&amp;byline=0&amp;title=0&amp;muted=1&amp;potrait=0&amp;transparent=0" style="width: 100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="" style="border-radius: 20px"></iframe>
+                    </div>
+                </div>
+            </div>
+
+                    <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-12 mt-7">
+                    <img src="../img/trabajos/swissmedical_marcaempleadora-3.jpg" class="img-fluid">
+                    </div>
+                    </div>
+                    </div>
+
+                    <div class="container">
+                    <div class="row align-items-center mb-15">
+                    <div class="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 text-center mt-7 mb-15">
+                    <p class="text-white ">EL RESULTADO</p>
+                    <p class="text-white mb-0 txt-trabajo-sub">El nuevo enfoque marcó un antes y un después en la comunicación interna y externa de Swiss Medical. <br>La marca logró un diferencial visual y estratégico que potenció su atractivo como empleador y consolidó su posicionamiento en el universo corporativo argentino.</p>                     
+                    </div>
+
+                     <div class="col-10 offset-1 offset-md-0 col-md-6">
+                    <img src="../img/trabajos/swissmedical_marcaempleadora-5.png" class="img-fluid">
+                    </div>
+
+                    <div class="col-10 offset-1 offset-md-0 col-md-6 col-lg-5 offset-lg-1 col-xl-4 mt-7 mt-lg-0 text-center text-md-start">
+                   
+                    <p class="text-white mb-0">Alcanzamos el objetivo anual de seguidores, <b>¡a 6 meses de cerrar el año!</b></p>
+                    </div>
+                    
+                    <div class="col-10 offset-1 offset-md-0 col-lg-5 col-md-6 col-xl-4 offset-xl-1 mt-7 mt-lg-0 order-2 order-md-1 text-center text-md-start">
+                   
+                    <p class="text-white mb-0">El objetivo propuesto fue construido de acuerdo a los resultados históricos del año 2024 y la proyección que arrojaba su tendencia.
+                    </p>
+                    </div>
+
+                    <div class="col-10 offset-1 offset-md-0 col-md-6 offset-lg-1 offset-xl-1 mt-7 mt-lg-0 order-1 order-md-2">
+                    <img src="../img/trabajos/swissmedical_marcaempleadora-6.png" class="img-fluid">
+                    </div>
+
+                    
+
+                   
+
+                    
+                    </div>
+                    </div>
+
+                   
+
+                    
+
+                          
+
+        </section>
+
+        <?php include_once("../templates/trabajos/anterior-siguiente-section.php"); ?>
+
+        <div id="footer-spacer" aria-hidden="true"></div>
+    </main>
+
+    <?php include_once("../templates/home/footer.php"); ?>
+
+   
+
+    <script defer src="../js/jquery.min.js"></script>
+    <script defer src="../js/bootstrap.min.js"></script>
+    <script defer src="../js/waypoint.js"></script>
+    <script defer src="../js/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/gsap.js"></script>
+    <script defer src="../js/smoothscroll-varticle.js"></script>
+    <script src="../js/smoothscroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <script defer src="../js/scrolltoplugin.js"></script>
+    <script defer src="../js/splittext.js"></script>
+    <script defer src="../js/counterup.js"></script>
+    <script defer src="../js/waw.js"></script>
+    <script defer src="../js/main.js"></script>
+
+    <script>
+        const menuToggle = document.getElementById("menuToggle");
+        const mobileNav = document.getElementById("mobileNav");
+        const mobileLinks = mobileNav.querySelectorAll("a");
+        const header = document.querySelector("header");
+
+        menuToggle.addEventListener("click", () => {
+            menuToggle.classList.toggle("active");
+            mobileNav.classList.toggle("open");
+        });
+
+        mobileLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                menuToggle.classList.remove("active");
+                mobileNav.classList.remove("open");
+            });
+        });
+
+        const mediaQuery = window.matchMedia("(max-width: 992px)");
+
+        function handleScroll() {
+            if (mediaQuery.matches) {
+                if (window.scrollY > 50) {
+                    header.classList.add("scrolled");
+                } else {
+                    header.classList.remove("scrolled");
+                }
+            } else {
+                header.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("resize", handleScroll);
+        handleScroll();
+
+        function ajustarAlturaFooter() {
+            const footer = document.querySelector("footer.footer");
+            const main = document.querySelector("main");
+            if (footer && main) {
+                const footerHeight = footer.offsetHeight;
+                main.style.marginBottom = footerHeight + "px";
+            }
+        }
+
+        window.addEventListener("load", ajustarAlturaFooter);
+        window.addEventListener("resize", ajustarAlturaFooter);
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const items = document.querySelectorAll("footer .fade-item");
+
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add("visible");
+                        }
+                    });
+                },
+                {
+                    root: null,
+                    threshold: 0.1,
+                }
+            );
+
+            items.forEach((item) => observer.observe(item));
+        });
+    </script>
+</body>
+</html>
